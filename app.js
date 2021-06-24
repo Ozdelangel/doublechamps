203cd2e043eeeee083405194b208b3c220e86310
OsbornDelAngel <ozdelangel@gmail.com>
2021-06-24 08:32:30 -0700

    new functions added and app2 gone

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-let userName = prompt('please enter your name:');
-console.log(userName);
-document.write('<h2>' + userName + '</h2>');
-
-let userAnswer = prompt('DO you like MMA?:')
-console.log(userAnswer);
-
-if (userAnswer == 'yes') {
-    document.write('<h3>Welcome ' + userName +'</h3>');
+let user1; 
+function getuserName(){
+    let userName = prompt('please enter your name:');
+    console.log(userName);
+    user1= userName
+    return (userName);
 }
- else {
-    document.write('<h3>' + userName +' , you should try it out</h3>');
+
+console.log(user1);
+
+function getuserAnswer(){
+    let userAnswer = prompt('DO you like MMA?:')
+    console.log(userAnswer);
+    
+    if (userAnswer.toLowerCase() == 'yes') {
+        document.write('<h3>Welcome ' + user1 +'</h3>') ;
+    } else {
+        document.write('<h3>' + user1 +' , you should try it out</h3>');
+    }
 }
+//getuserAnswer();
+
+function getbeltanswer(){
+let userAnswer = prompt('Do you want to see how the current UFC belt looks like?');
+
+let url ="https://mmajunkie.usatoday.com/wp-content/uploads/sites/91/2019/01/henry-cejudo-ufc-brooklyn-post-fight-4.jpg?w=1000&h=576&crop=1"
+document.write('<img src="' + url + '"/>')
+if (userAnswer.toLowerCase() == 'no') {
+    let url ='<img src="https://i.imgflip.com/1jmzdm.jpg"/>'
+    document.write(url)
+}
+} 
+
+
+
+
+
+
+
+
+
+
+ 
